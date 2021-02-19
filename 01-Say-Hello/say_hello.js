9aa94717ff54555973c1bd257b0289b2473ae568
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-19 11:11:32 +0100

    Finished with the first exercise of today

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -3,6 +3,22 @@
 
 here
 
+
+- Start
+- define a varibale
+- prompt user to input his name
+- save it on the created varibale
+- print the greetings including the name of the user
+- finish
+
 */
 
 //insert your code below
+
+var Nam, Hello, x;
+
+
+Nam = window.prompt('abc');
+Hello = 'Hello ';
+x = ', How are you doing today?';
+window.alert(Hello + Nam + x);
