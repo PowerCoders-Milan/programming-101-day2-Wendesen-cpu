c61e7b111792a80764670d448ccf93a9b7462887
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-19 12:01:06 +0100

    Done with this exercise

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,26 @@
 //insert your pseudocode below
 /*
 
+start
+define two  variables
+prompt the user to input the string
+save the input in the first variable
+calculate the length of the string
+save the value in the second varibale
+display the string with the length
+finish
+
+
 here
 
 */
 
 //insert your code below
+
+var input, x, Nam;
+
+
+input = window.prompt('Input the string you want');
+x = input.length;
+Nam = ': ';
+window.alert(input + Nam + x);
